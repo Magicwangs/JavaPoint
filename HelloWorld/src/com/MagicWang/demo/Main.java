4f0d0758deadd42aa34619d9a2801134b3d4747f
王灏 <752106129@qq.com>
2016-10-12 20:52:44 +0800

    Java Study Begins

---
+++ HelloWorld/src/com/MagicWang/demo/Main.java
@@ -0,0 +1,10 @@
+package com.MagicWang.demo;
+
+public class Main {
+
+    public static void main (String[] args) {
+	// write your code here
+        System.out.println("你好!"+(7%5.5));//打印HelloWorld
+        System.out.println("你好!"+(7%5.2));//打印HelloWorld
+    }
+}
